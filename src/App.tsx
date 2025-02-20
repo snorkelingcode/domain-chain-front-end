59103f5a3e71bbdefd79fb16cd383db5bbda7c59
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-20 16:07:39 -0600

    tabs centered

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto px-4">
-        <div className="flex justify-center items-center h-16 my-8 relative">
-          <div className="inline-flex rounded-md shadow-sm" role="group">
+       <div className="flex justify-center items-center h-16 my-8">
+          <div className="inline-flex rounded-md shadow-sm mx-auto" role="group">
             <button 
               type="button"
               onClick={() => setMode('buy')}
