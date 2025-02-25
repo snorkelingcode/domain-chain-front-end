b36153e85473550d7c1ed1884be3589cd62c67e0
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-24 20:21:38 -0600

    updated index.html to be more permissive

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
   server: {
     headers: {
       'Content-Security-Policy': [
         "default-src 'self'",
         "script-src 'self'",
         "style-src 'self' 'unsafe-inline'",
-        "connect-src 'self' 'https://connect.walletconnect.org'",
-        "img-src 'self' 'https://explorer-api.walletconnect.com' 'https://thirdweb.com' 'https://*.thirdweb.com' 'https://thirdweb-cdn.com' 'https://*.thirdweb-cdn.com' 'data:' 'https://domainchain.link'",
+        "connect-src 'self' https://connect.walletconnect.org https://explorer-api.walletconnect.com",
+        "img-src 'self' https://explorer-api.walletconnect.com data: https://thirdweb.com https://*.thirdweb.com https://thirdweb-cdn.com https://*.thirdweb-cdn.com",
         "frame-ancestors 'none'",
       ].join("; "),
       'X-Frame-Options': 'DENY',
